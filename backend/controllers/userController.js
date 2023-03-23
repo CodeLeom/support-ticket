b70c270d40bbc9613876e01c575b04d6067ab3f1
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-03-23 19:36:32 +0100

    added Async Error Handling Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,9 @@
+const asyncHandler = require('express-async-handler')
 
 // @desc        Register a new user
 // @route       /api/users
 // @access      Public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     const {name, email, password} = req.body
 
     //validation
@@ -12,16 +13,15 @@ const registerUser = (req, res) => {
     }
 
     res.send('Register Route')
-}
+})
 
 
 // @desc        Login a user
 // @route       /api/users/login
 // @access      Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.send('Login Route')
-}
-
+})
 
 module.exports = {
     registerUser,
