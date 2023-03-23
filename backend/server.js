d3d17e313d3ccb542983d3e5744f3bf525a3e3a7
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-03-23 19:23:27 +0100

    set the error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 const PORT = process.env.PORT || 8000
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.send({message: 'Welcome to the support desk API'})
@@ -12,5 +16,7 @@ app.get('/', (req, res) => {
 //routes
 app.use('/api/users', require('./routes/userRoutes'))
 
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
