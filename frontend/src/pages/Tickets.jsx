010d299df6b841ea14b6b5137cbd7c67d7906316
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-04-06 16:18:57 +0100

    List tickets on tickets page

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { getTickets, reset } from "../features/tickets/ticketSlice"
 import Spinner from "../components/Spinner"
 import BackButton from "../components/BackButton"
+import TicketItem from "../components/TicketItem"
 
 function Tickets() {
     const {tickets, isLoading, isSuccess} = useSelector((state) => state.tickets)
@@ -24,9 +25,23 @@ function Tickets() {
         return <Spinner />
     }
   return (
-    <div>
-      <h1>Tickets</h1>
-    </div>
+    <>
+         <BackButton url={'/'} />
+             <h1>Tickets</h1>
+             <div className="tickets">
+                <div className="ticket-headings">
+                    <div>Date</div>
+                    <div>Product/Issues</div>
+                    <div>Status</div>
+                    <div></div>
+                </div>
+                    <div>
+                        {tickets.map((ticket) => (
+                            <TicketItem key={ticket._id} ticket={ticket} />
+                        ))}
+                    </div>
+             </div>
+    </>
   )
 }
 
