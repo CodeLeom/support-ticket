64e33d447a8e657e878e6a8e383ddfe24b27d26f
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-04-12 10:38:31 +0100

    path fix

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,9 @@ app.use('/api/tickets', require('./routes/ticketRoutes'))
 // serve frontend
 if(process.env.NODE_ENV === 'production'){
     // set build folder as static
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.use(express.static(path.join(__dirname, 'frontend', 'build')))
 
-    app.get('*', (req, res) => res.sendFile(__dirname, '../frontend', '/build', '/index.html'))
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html')))
 } else {
     app.get('/', (req, res) => {
         res.send({message: 'Welcome to the support desk API'})
